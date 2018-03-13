7005d221b41f879a648eaf5a76a06982176a4f71
Jonathan Van Buren <jvburen@thoughtworks.com>
2018-03-13 09:54:44 +0800

    trying to cache stuff

--- src/data/queries/news.js
+++ src/data/queries/news.js
@@ -17,43 +17,25 @@ const url =
   '?rss_url=https%3A%2F%2Freactjsnews.com%2Ffeed.xml';
 
 let items = [];
-let lastFetchTask;
-let lastFetchTime = new Date(1970, 0, 1);
 
 const news = {
   type: new List(NewsItemType),
-  resolve() {
-    if (lastFetchTask) {
-      return lastFetchTask;
-    }
-
+  resolve(_, _1, _2, { cacheControl }) {
     const tenMinutes = 1000 * 60 * 10;
+    cacheControl.setCacheHint({ maxAge: tenMinutes });
 
-    if (new Date() - lastFetchTime > tenMinutes) {
-      lastFetchTime = new Date();
-      lastFetchTask = fetch(url)
-        .then(response => response.json())
-        .then(data => {
-          if (data.status === 'ok') {
-            items = data.items;
-          }
+    return fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        if (data.status === 'ok') {
+          items = data.items;
+        }
 
-          lastFetchTask = null;
-          return items;
-        })
-        .catch(err => {
-          lastFetchTask = null;
-          throw err;
-        });
-
-      if (items.length) {
         return items;
-      }
-
-      return lastFetchTask;
-    }
-
-    return items;
+      })
+      .catch(err => {
+        throw err;
+      });
   },
 };
 
