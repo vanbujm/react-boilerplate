4cb6805045fafba88e57e25f19d1157a36f0640d
Jonathan Van Buren <jvburen@thoughtworks.com>
2018-03-22 15:52:47 +0800

    messing around with config3

--- src/data/database.js
+++ src/data/database.js
@@ -9,7 +9,13 @@ const config = databaseConfig[env];
 
 export function createSequelizeConnection(configObject) {
   return configObject.use_env_variable
-    ? new Sequelize(process.env[configObject.use_env_variable], configObject)
+    ? new Sequelize(process.env[configObject.use_env_variable], {
+        dialect: 'postgres',
+        protocol: 'postgres',
+        port: match[4],
+        host: match[3],
+        logging: true, // false
+      })
     : new Sequelize(
         configObject.database,
         configObject.username,
